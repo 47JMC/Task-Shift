fc422ed16588f364fe678305063093356862d2f0
47JMC <mjabir.5b@gmail.com>
2024-10-28 17:09:02 +0530

    Added home page to use the previous manage route

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import Link from "next/link";
+import TasksInput from "./components/TasksInput";
+import TaskList from "./components/Task";
 
 function page() {
   return (
     <>
-      <h1 className=" text-center m-1 text-4xl font-bold">TaskShift</h1>
-      <p className="font-semibold text-2xl m-2">
-        The easiest way to <span className="bg-blue-800">manage tasks.</span>
-      </p>
-      <Link href="/manage">
-        <button className="p-1 w-35 h-18 hover:bg-green-700 m-2 text-center rounded-lg bg-green-600">
-          Manage Tasks
-        </button>
-      </Link>
+      <h1 className="font-bold text-center text-2xl m-1">TaskShift</h1>
+      <TasksInput />
+      <TaskList />
     </>
   );
 }
