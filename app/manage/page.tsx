6d8a3e5c757ed989d7e456198cf27848aee5acbd
47JMC <mjabir.5b@gmail.com>
2024-10-28 17:04:55 +0530

    Added simple home page

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -4,7 +4,7 @@ import TaskList from "../components/Task";
 function page() {
   return (
     <>
-      <h1 className="font-semibold text-center text-2xl m-1">TaskShift</h1>
+      <h1 className="font-bold text-center text-2xl m-1">TaskShift</h1>
       <TasksInput />
       <TaskList />
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 function page() {
-  return <div></div>;
+  return (
+    <>
+      <h1 className=" text-center m-1 text-4xl font-bold">TaskShift</h1>
+      <p className="font-semibold text-2xl m-2">
+        The easiest way to <span className="bg-blue-800">manage tasks.</span>
+      </p>
+      <Link href="/manage">
+        <button className="p-1 w-35 h-18 hover:bg-green-700 m-2 text-center rounded-lg bg-green-600">
+          Manage Tasks
+        </button>
+      </Link>
+    </>
+  );
 }
 
 export default page;
